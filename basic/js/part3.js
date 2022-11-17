37ed377db17bf9407adc2a2472426290735af5fd
cozy-ant <koreanant93@gmail.com>
2022-11-18 00:05:21 +0900

    study querySelector

---
+++ basic/js/part3.js
@@ -0,0 +1,8 @@
+//3.0 document 객체.
+//----------------------------------
+//3.1 document.getElementById()
+//----------------------------------
+//3.2 querySelector && querySelectorAll
+// id, class 네임을 각각 들고오는 것 보다 querySelector를 사용하는게 더 효율적이고 편리
+// CSS 셀렉터 
+//----------------------------------
