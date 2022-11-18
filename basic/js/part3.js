55cb67b7f9b0b8bca7ad368e3c5fb786ab7fe849
cozy-ant <koreanant93@gmail.com>
2022-11-19 00:12:32 +0900

    study addEventListener

--- basic/js/part3.js
+++ basic/js/part3.js
@@ -4,5 +4,22 @@
 //----------------------------------
 //3.2 querySelector && querySelectorAll
 // id, class 네임을 각각 들고오는 것 보다 querySelector를 사용하는게 더 효율적이고 편리
-// CSS 셀렉터 
+// CSS 셀렉터
 //----------------------------------
+//3.3 ~ 3.5 addEventListener
+// const title = document.querySelector("h1");
+// title.addEventListener("click", () =>  console.log("Click title") );
+// title.addEventListener("mouseenter", () =>  console.log("ClickEnter title") );
+// title.addEventListener("mouseleave", () =>  console.log("onMouseLeave title") );
+// console.log(title);
+// title.onclick = () => {
+//   title.style.color = "blue";
+//   console.log('Click title');
+// }
+// title.addEventListener("click", () => { title.style.fontSize = "18px" });
+// window.addEventListener("resize", () => { document.body.style.backgroundColor = "tomato" })
+// window.addEventListener("copy", () => { alert("Don't copy")});
+// window.addEventListener("online", () => { alert("online") });
+// window.addEventListener("offline", () => { alert("offline") });
+//----------------------------------
+// 3.6
