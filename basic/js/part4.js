8441c7e7193436e71967bbcae96da18badd912e2
cozy-ant <koreanant93@gmail.com>
2022-11-30 00:37:33 +0900

    localStorage && submit

--- basic/js/part4.js
+++ basic/js/part4.js
@@ -1,8 +1,3 @@
-const loginForm = document.querySelector(".login-form");
-
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
-
 // loginButton.addEventListener("click", () => {
 //   const userName = loginInput.value;
 //   if (!userName) {
@@ -17,3 +12,45 @@ const loginButton = loginForm.querySelector("button");
 //   const userName = loginInput.value;
 //   console.log(userName)
 // })
+//preventDefault 로 새로고침 막기 a태그에서도 사용
+// loginForm.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   // const userName = loginInput.value;
+//   console.log(loginInput.value);
+// })
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+const title = document.querySelector("h1");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "userName";
+
+localStorage.clear()
+
+const showText = (userName) => {
+  title.textContent = `Hello ${userName}`;
+  loginForm.classList.add(HIDDEN_CLASSNAME)  
+}
+
+const onLoginSubmit = (event) => {
+  event.preventDefault();
+  const userName = loginInput.value;
+
+  localStorage.setItem(USERNAME_KEY, userName);
+  
+  title.classList.remove(HIDDEN_CLASSNAME)
+
+  showText(userName);
+  
+  console.log(userName)
+};
+
+const getStorageKey = localStorage.getItem(USERNAME_KEY);
+
+if (getStorageKey === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener('submit', onLoginSubmit)
+} else {
+  showText(getStorageKey);
+}
