95c5dd4a8a11bbbc29e28d64a3747d45832fbf31
cozy-ant <koreanant93@gmail.com>
2023-01-26 00:18:46 +0900

    create todo list

--- basic/js/todo.js
+++ basic/js/todo.js
@@ -1,32 +1,57 @@
 const form = document.querySelector(".input-wrap");
 const input = document.querySelector(".input");
-const list = document.querySelector(".list-wrap");
+const lists = document.querySelector(".list-wrap");
+
+let arr = [];
+const KEY = "list";
 
 function deleteTodo(e) {
+  const data = JSON.parse(localStorage.getItem(KEY))
   const list = e.target.parentElement;
+  arr = data.filter(v => v.id !== parseInt(list.id));
   list.remove();
+  saveLocal();
 }
 
-function addTodo(todo) {
+function addTodo(props) {
+  const { data, id } = props
   const li = document.createElement("li");
+  li.id = id;
   const span = document.createElement("span");
-  span.innerText = todo;
+  span.innerText = data;
+
   const button = document.createElement("button");
   button.innerText = "X"
 
-  button.addEventListener("click", deleteTodo)
   li.appendChild(span);
   li.appendChild(button);
-  
-  list.appendChild(li);
+  lists.appendChild(li);
+
+  button.addEventListener("click", deleteTodo)
+}
+
+const item = JSON.parse(localStorage.getItem(KEY));
+if (item) {
+  item.forEach(v => {
+    arr.push(v)
+    addTodo(v);
+  })
 }
 
 function handleSubmit(e) {
   e.preventDefault();
-  const todo = input.value;
+  const todo = {
+    id: Date.now(),
+    data: input.value
+  };
+  arr.push(todo);
   input.value = '';
   addTodo(todo);
+  saveLocal();
+}
+
+function saveLocal() {
+  localStorage.setItem(KEY, JSON.stringify(arr))
 }
 
 form.addEventListener("submit", handleSubmit)
-
